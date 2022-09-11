0f96aa6b8b7f5936b14cb592f705e7a5cc1e2f95
patricksulli76 <113055515+patricksulli76@users.noreply.github.com>
2022-09-11 16:34:56 -0400

    Create Player.java

---
+++ crypt-escape/core/src/com/cryptescape/game/Player.java
@@ -0,0 +1,60 @@
+package com.cryptescape.game;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Player extends Movables{
+    public boolean isRunning = false;
+
+    public Player(double x,double y){
+        super(x, y, 2.0, 8.0, "Player.png");
+    }
+    public TextureRegion spriteStage() {
+    	if (spritePos < width) {
+			spritePos +=1;
+		} else {
+			spritePos = 0;
+		}
+		if ((vel[0] > 0) && (vel[1] == 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,0,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] < 0) && (vel[1] == 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,0,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] == 0) && (vel[1] > 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,32,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] == 0) && (vel[1] < 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,64,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] > 0) && (vel[1] > 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,96,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] > 0) && (vel[1] > 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,96,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] < 0) && (vel[1] > 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,96,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] > 0) && (vel[1] < 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,128,32,32);
+			return spriteRegion;
+		}
+		else if ((vel[0] < 0) && (vel[1] < 0)){
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,128,32,32);
+			return spriteRegion;
+		}
+		else {
+			spriteRegion = new TextureRegion(spriteMap, 0+32*spritePos,160,32,32);
+			return spriteRegion;
+		}
+		
+	}
+
+}
