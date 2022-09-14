422a6edeb1f9f195300ae2d0631169be27705fa6
TonyPlaysGames <47358504+TonyPlaysGames@users.noreply.github.com>
2022-09-13 21:25:26 -0400

    Update Player.java

--- crypt-escape/core/src/com/cryptescape/game/Player.java
+++ crypt-escape/core/src/com/cryptescape/game/Player.java
@@ -12,7 +12,7 @@ public class Player extends Movables {
 	private float counter = 0f;
 	private boolean changeAnimation = true;
 	public Player(double x, double y, Texture t) {
-		super(x, y, 1.0, 5.0, t);
+		super(x, y, 1.0, 3.0, t);
 		spriteRegion = new TextureRegion(spriteMap);
 	}
 
@@ -22,38 +22,38 @@ public class Player extends Movables {
 		//Gdx.graphics.getDeltaTime();
 		if (changeAnimation == true) {
 			changeAnimation = false;
-			if (spritePos < width) {
+			if (spritePos+1 < width) {
 				spritePos += 1;
 			} else {
 				spritePos = 0;
 			}
 			
 			if ((vel[0] > 0) && (vel[1] == 0)) { // East
-				spriteRegion.setRegion(0 + 32 * spritePos, 0, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 32, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] < 0) && (vel[1] == 0)) { // West
-				spriteRegion.setRegion(0 + 32 * spritePos, 0, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 96, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] == 0) && (vel[1] > 0)) { // North
-				spriteRegion.setRegion(0 + 32 * spritePos, 32, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 0, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] == 0) && (vel[1] < 0)) { // South
 				spriteRegion.setRegion(0 + 32 * spritePos, 64, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] > 0) && (vel[1] > 0)) { // Northwest
-				spriteRegion.setRegion(0 + 32 * spritePos, 96, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 128, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] > 0) && (vel[1] < 0)) { // Southwest
-				spriteRegion.setRegion(0 + 32 * spritePos, 128, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 224, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] < 0) && (vel[1] > 0)) { // Northeast
-				spriteRegion.setRegion(0 + 32 * spritePos, 96, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 160, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] < 0) && (vel[1] < 0)) { // Southeast
-				spriteRegion.setRegion(0 + 32 * spritePos, 128, 32, 32);
+				spriteRegion.setRegion(0 + 32 * spritePos, 192, 32, 32);
 				return spriteRegion;
 			} else if ((vel[0] == 0) && (vel[1] == 0)) { // Standing still 
-				// dosomething
+				spriteRegion.setRegion(0, 64, 32, 32);
 			}
 			
 		}
