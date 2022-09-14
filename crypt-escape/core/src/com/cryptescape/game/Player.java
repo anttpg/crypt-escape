35e371707052c6985cb1119606ed57838eddeb21
TonyPlaysGames <47358504+TonyPlaysGames@users.noreply.github.com>
2022-09-13 22:37:53 -0400

    Update Player.java

--- crypt-escape/core/src/com/cryptescape/game/Player.java
+++ crypt-escape/core/src/com/cryptescape/game/Player.java
@@ -28,30 +28,30 @@ public class Player extends Movables {
 				spritePos = 0;
 			}
 			
-			if ((vel[0] > 0) && (vel[1] == 0)) { // East
+			if ((vel[0] >= 1) && ((vel[1] <= 1) && (vel[1] >= -1)) ) { // East
 				spriteRegion.setRegion(0 + 32 * spritePos, 32, 32, 32);
-				return spriteRegion;
-			} else if ((vel[0] < 0) && (vel[1] == 0)) { // West
+				
+			} else if ((vel[0] <= -1) && ((vel[1] <= 1) && (vel[1] >= -1)) ) { // West
 				spriteRegion.setRegion(0 + 32 * spritePos, 96, 32, 32);
-				return spriteRegion;
-			} else if ((vel[0] == 0) && (vel[1] > 0)) { // North
+				
+			} else if ( ((vel[0] <= 1) && (vel[0] >= -1) ) && (vel[1] >= 1)) { // North
 				spriteRegion.setRegion(0 + 32 * spritePos, 0, 32, 32);
-				return spriteRegion;
-			} else if ((vel[0] == 0) && (vel[1] < 0)) { // South
+				
+			} else if ( ((vel[0] <= 1) && (vel[0] >= -1) ) && (vel[1] <= -1)) { // South
 				spriteRegion.setRegion(0 + 32 * spritePos, 64, 32, 32);
-				return spriteRegion;
+				
 			} else if ((vel[0] > 0) && (vel[1] > 0)) { // Northwest
 				spriteRegion.setRegion(0 + 32 * spritePos, 128, 32, 32);
-				return spriteRegion;
+				
 			} else if ((vel[0] > 0) && (vel[1] < 0)) { // Southwest
-				spriteRegion.setRegion(0 + 32 * spritePos, 224, 32, 32);
-				return spriteRegion;
+				spriteRegion.setRegion(0 + 32 * spritePos, 192, 32, 32);
+				
 			} else if ((vel[0] < 0) && (vel[1] > 0)) { // Northeast
 				spriteRegion.setRegion(0 + 32 * spritePos, 160, 32, 32);
-				return spriteRegion;
+				
 			} else if ((vel[0] < 0) && (vel[1] < 0)) { // Southeast
-				spriteRegion.setRegion(0 + 32 * spritePos, 192, 32, 32);
-				return spriteRegion;
+				spriteRegion.setRegion(0 + 32 * spritePos, 224, 32, 32);
+				
 			} else if ((vel[0] == 0) && (vel[1] == 0)) { // Standing still 
 				spriteRegion.setRegion(0, 64, 32, 32);
 			}
