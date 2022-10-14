621bedcad47be91a72b4bc340c23720bbf398505
TonyPlaysGames <47358504+TonyPlaysGames@users.noreply.github.com>
2022-10-14 18:00:46 -0400

    Finished room preset generation

--- crypt-escape/core/src/com/cryptescape/game/RoomGeneration.java
+++ crypt-escape/core/src/com/cryptescape/game/RoomGeneration.java
@@ -20,7 +20,7 @@ public class RoomGeneration {
 		}
 		
 		// use ##.next() to get where in key[i] to use
-		String[][] seed = createEmptyNxN(Constants.Y_TILES, Constants.X_TILES, false);
+		String[][] seed = createEmptyNxN(Constants.Y_TILES, Constants.X_TILES, false, false);
 		
 		double[] p2 = new double[] {75, 5, 2, 2, 2, 2, 6, 6}; // Probability of room type
 		String[] key2 = new String[] {"open", "blocked", "bN3", "bE3", "bS3", "bW3", "bN1", "bE1", "bS1", "bW1", "hallNS", "hallEW", "cb"}; 
@@ -45,8 +45,12 @@ public class RoomGeneration {
 		pregenTemplate.add(addBlock3x3(seed, 1, 1)); // cb | Center blocked
 		
 		for(int i = 0; i < pregenTemplate.size(); i++) {
-			pregenTemplate.set(i, removeUselessWalls(pregenTemplate.get(i)));
+			System.out.println("BEFORE REMOVING WALLS: ");
 			printSeedArray(pregenTemplate.get(i), Integer.toString(i));
+			
+			System.out.println("AFTER REMOVING WALLS: ");
+			pregenTemplate.set(i, removeUselessWalls(pregenTemplate.get(i)));
+			printSeedArray(pregenTemplate.get(i), Integer.toString(i) + ": " +key2[i]);
 		}
 		
 		final ArrayList<String[][]> TEMPLATE = new ArrayList<String[][]>(Collections.unmodifiableList(pregenTemplate)); //makes it unmodable
@@ -113,17 +117,21 @@ public class RoomGeneration {
 	 * Combinds two arrays, so long as original + offset > toCombind.length for both axis.
 	 */
 	private static String[][] combindArray(String[][] original, String[][] toCombind, int colStart, int rowStart) {
-//		try {
+		int colCount = 0;
+		int rowCount = 0;
+		try {
 			for(int col = colStart; col < toCombind.length + colStart; col++) {
-				for(int row = rowStart; row < toCombind[col].length + rowStart; row++) {
+				colCount = col;
+				for(int row = rowStart; row < toCombind[col-colStart].length + rowStart; row++) {
+					rowCount = row;
 					original[col][row] = new String(toCombind[col-colStart][row-rowStart]);
 				}
 			}
-//		} 
-//		catch(Exception ArrayIndexOutOfBoundsException) {
-//			System.out.println("Error: the size of Original + Offset vs toCombind is out of bounds. \n Original: " 
-//		+ original.length + "   toCombind: " + toCombind.length + "     col: " + colStart + "   row: "+ rowStart);			
-//		}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Error: the size of Original + Offset vs toCombind is out of bounds. \n Original: " 
+		+ original.length + "   toCombind: " + toCombind.length + "     col: " + colStart + "   row: "+ rowStart);			
+		}
 		return original;
 	}
 	
@@ -132,18 +140,32 @@ public class RoomGeneration {
 	 * original: the seed to apply it to.
 	 */
 	private static String[][] createNxN(String[][] original, boolean[][] map) {
-		for(int col = 0; col < map.length; col++) {
+		original = createEmptyNxN(original.length, original[0].length, false, false);
+		
+		for(int col = 0; col < map.length; col++) { //Block additions
 			for(int row = 0; row < map[col].length; row++) {
 				if(map[col][row]) {
 					if(map.length == 3) {
 						original = addBlock3x3(original, col, row);
 					}
 					else {
-						
+						//SomethingElse
 					}
 				}
 			}
 		}
+		
+		String[][] temp = createEmptyNxN(original.length, original[0].length, false, false);
+		for(int col = 0; col < original.length; col++) { //Edges redone to override blocks
+			for(int row = 0; row < original[col].length; row++) {
+				if(row == 0 || row == Constants.X_TILES || col == 0 || col == Constants.Y_TILES) {
+					original[col][row] = temp[col][row];
+					
+				}
+			}
+		}
+		
+		
 		return original;
 	}
 	
@@ -154,7 +176,7 @@ public class RoomGeneration {
 	private static String[][] addBlock3x3(String[][] original, int col, int row) {
 		int height = (Constants.Y_TILES)/3;
 		int width = (Constants.X_TILES)/3;
-		String[][] combind = createEmptyNxN(height, width, true);
+		String[][] combind = createEmptyNxN(height, width, true, true);
 		
 		int colStart = (col * Constants.Y_TILES)/3;
 		int rowStart = (row * Constants.X_TILES)/3;
@@ -168,44 +190,45 @@ public class RoomGeneration {
 	 * Keep col & row > 5 to prevent weird bugs
 	 * If block is TRUE, all squares inside will be blocked. Otherwise they will be empty.
 	 */
-	private static String[][] createEmptyNxN(int col, int row, boolean block) {
+	private static String[][] createEmptyNxN(int col, int row, boolean block, boolean isGrid) {
 		String[][] s = new String[col][row];
 		for(int y = 0; y < col; y++) { //Loop through and fill the boundaries
 			for(int x = 0; x < row; x++) {
+				
 				if(block) s[y][x] = "blocked";
 				else s[y][x] = "empty";
 				
 				//NORTH-SOUTH
 				if(y == 0) { //SOUTH FACING
-					if(x == (row/2) || x == (row/2)-1) // If doorway
-						s[y][x] = "southDoor";
+					if((x == (row/2) || x == (row/2)-1) && !isGrid) // If doorway
+						s[y][x] = "northDoor";
 					else  // regular wall
-						s[y][x] = "southWall";		
+						s[y][x] = "northWall";		
 				}
 				else if(y == col-1) { // NORTH FACING
-					if(x == (row/2) || x == (row/2)-1) 
-						s[y][x] = "northDoor";
+					if((x == (row/2) || x == (row/2)-1) && !isGrid)  //isGrid just checks if the square is empty. If so, no doors are generated.
+						s[y][x] = "southDoor";
 					else  
- 						s[y][x] = "northWall";
+ 						s[y][x] = "southWall";
 				}
 				
 				//EAST-WEST
 				else if(x == 0) { //WEST FACING
-					if(y == (col/2) || y == (col/2)-1) // If doorway
+					if((y == (col/2) || y == (col/2)-1) && !isGrid) // If doorway
 						s[y][x] = "westDoor";
 					else  // regular wall
 						s[y][x] = "westWall";
 					
 				}
 				else if(x == row-1) { //EAST FACING
-					if(y == (col/2) || y == (col/2)-1) 
+					if((y == (col/2) || y == (col/2)-1) && !isGrid) 
 						s[y][x] = "eastDoor";
 					else  
  						s[y][x] = "eastWall";
 				}
 				
 
-				if((x == 0 || x == (row-1)) && (y == 0 || y == col-1)) { //Corners
+				if((x == 0 || x == (row-1)) && (y == 0 || y == col-1) && !isGrid) { //Corners (Minus blocked segments)
 					s[y][x] = "blocked";
 				}
 			}
@@ -220,15 +243,26 @@ public class RoomGeneration {
 	 */
 	
 	private static String[][] rotateClockWise90(String[][] matrix) {
-		 int sizeY = matrix.length;
-		 int sizeX = matrix[1].length;
-		 String[][] ret = clone2dArray(matrix);
+		int sizeY = matrix.length;
+		int sizeX = matrix[1].length;
+		boolean switchNext = false;
+		String[][] ret = clone2dArray(matrix);
 
-		 for (int i = 1; i < sizeY-1; ++i) 
-		  for (int j = 1; j < sizeX-1; ++j) 
-		   ret[i][j] = matrix[sizeX - j - 1][i]; //***
-		 
-		 return ret;
+		for (int i = 1; i < sizeY - 1; ++i) {
+			for (int j = 1; j < sizeX - 1; ++j) {
+				ret[i][j] = matrix[sizeX - j - 1][i]; //Rotates array minus edges
+				
+				//Just checks to see if wall side need to switch TODO
+//				for(String w : Constants.WALLTYPES) {
+//					if(switchNext) { 
+//						switchNext = false;
+//						ret[i][j] = w;
+//					}
+//					if(ret[i][j].equals(w)) switchNext = true;
+//				}
+			}
+		}
+		return ret;
 	}
 	
 	
@@ -250,31 +284,53 @@ public class RoomGeneration {
 	private static String[][] removeUselessWalls(String[][] r) {
 		for(int y = 0; y < r.length; y++) {
 			for(int x = 0; x < r[y].length; x++) {
-				if(x != 0 && (x+1 < r[y].length)) {
-					if(r[y][x-1].equals("blocked") && r[y][x+1].equals("blocked")) {
-						r[y][x] = "blocked";
-					}
-				}
-				else if(x == 0 && r[y][x+1].equals("blocked")) r[y][x] = "blocked";
-				else if(x == r[y].length && r[y][x-1].equals("blocked")) r[y][x] = "blocked";
 				
-				if(y != 0 && (y+1 < r.length)) {
-					if(r[y-1][x].equals("blocked") && r[y+1][x].equals("blocked")) {
-						r[y][x] = "blocked";
+				if(x != 0 && (x+1 < r[y].length)) { //Prevents out of bounds x
+					if(inaccessable(r[y][x-1]) && inaccessable(r[y][x+1])) {  //Checks x sides
+						
+						if(y != 0 && (y+1 < r.length)) { //Prevents out of bounds y
+							if(inaccessable(r[y-1][x]) && inaccessable(r[y+1][x])) {  //Checks y sides
+								r[y][x] = "blocked";
+							}
+						}
 					}
 				}
-				else if(y == 0 && r[y+1][x].equals("blocked")) r[y][x] = "blocked";
-				else if(y == r.length && r[y-1][x].equals("blocked")) r[y][x] = "blocked";
+				
+				if(x == 0 && inaccessable(r[y][x+1])) r[y][x] = "blocked";  //checks for x=0 edge cases
+				if(x+1 == Constants.X_TILES && inaccessable(r[y][x-1])) r[y][x] = "blocked"; 
+				
+				if(y == 0 && inaccessable(r[y+1][x])) r[y][x] = "blocked";  //checks for y=0 edge cases
+				if(y+1 == Constants.Y_TILES && inaccessable(r[y-1][x])) r[y][x] = "blocked";
 			}
 		}
 		return r;
 	}
 	
+	private static boolean inaccessable(String side) {
+		if(side.equals("blocked")){
+			return true;
+		}
+		for(String d : Constants.DOORTYPES) {
+			if(side.equals(d)) return true;
+		}
+		for(String w : Constants.WALLTYPES) {
+			if(side.equals(w)) return true;
+		}
+		
+		return false;
+	}
+	
 	public static void printSeedArray(String[][] org, String rt) {
 		System.out.println("Type of room should be: " + rt);
+		int count = 10;
 		for(String[] s : org) {
+			System.out.print("Col " + count + ":   ");
+			count++;
 			for(String s2 : s) {
-				System.out.print(s2 + " ");
+				for(String w : Constants.WALLTYPES)
+					if(s2.equals(w))
+						s2 = s2.toUpperCase();
+				System.out.print(s2.substring(0,5) + " ");
 			}
 			System.out.println();
 		}
